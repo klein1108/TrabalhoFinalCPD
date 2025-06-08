4ec80deb20c7755822f89ef3ca4272e3fc3f5eba
Caio Nunes <caioffnunes1610@gmail.com>
2025-06-07 22:18:30 -0300

    Otimizacao da MoviesHashTable

--- src/header/fileHeader.hpp
+++ src/header/fileHeader.hpp
@@ -8,7 +8,7 @@
 using namespace aria::csv;
 
 void printAllFileDataByName(char fileName[]);
-vector<Movie> setAllMoviesFromFileToVector();
+vector<Movie> setAllMoviesFromFileToVector(vector<unique_ptr<MovieHash>>& moviesHashTable);
 vector<Review> setAllReviewsFromFileToVector();
 vector<string> splitStringIntoNewVectorBySeparator(string s, string delimiter);
 
--- src/service/hashService.cpp
+++ src/service/hashService.cpp
@@ -1,6 +1,19 @@
 #include "../header/hashHeader.hpp"
 
-vector<unique_ptr<MovieHash>> createMoviesHashTable(vector<Movie> movies){
+void addToMoviesHashTable(vector<unique_ptr<MovieHash>>& hashTable, MovieHash movie)
+{
+  unique_ptr<MovieHash> movieHash = make_unique<MovieHash>();
+
+  movieHash->movie = movie.movie;
+
+  movieHash->next = nullptr;
+
+  insertPlayerInHashTableById(hashTable, movieHash);
+
+}
+
+vector<unique_ptr<MovieHash>> createMoviesHashTable(vector<Movie> movies)
+{
   vector<unique_ptr<MovieHash>> response(MAX_MOVIE_HASH);
   
   int greatestTableSize = 0;
@@ -11,10 +24,7 @@ vector<unique_ptr<MovieHash>> createMoviesHashTable(vector<Movie> movies){
     movieHash->next = nullptr;
 
     //Mapping Movie to MovieHash
-    movieHash->movieId = movies.at(i).movieId;
-    movieHash->title = movies.at(i).title;
-    movieHash->genres = movies.at(i).formatGenres;
-    movieHash->year = movies.at(i).year;
+    movieHash->movie = movies.at(i);
 
     int tableSize = insertPlayerInHashTableById(response, movieHash);
 
@@ -30,8 +40,9 @@ vector<unique_ptr<MovieHash>> createMoviesHashTable(vector<Movie> movies){
   return response;
 }
 
-int insertPlayerInHashTableById(vector<unique_ptr<MovieHash>>& hashTable, unique_ptr<MovieHash>& movieHash){
-  int module = (int) movieHash->movieId % MAX_MOVIE_HASH;
+int insertPlayerInHashTableById(vector<unique_ptr<MovieHash>>& hashTable, unique_ptr<MovieHash>& movieHash)
+{
+  int module = (int) movieHash->movie.movieId % MAX_MOVIE_HASH;
   int tableSize = 1;
 
   if(!hashTable.at(module)){
